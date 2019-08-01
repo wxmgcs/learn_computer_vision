9273d85bae91739af7e5ee9fca6be5f1c387dcd8
wxmgcs <wxmgcs@gmail.com>
2019-08-01 21:47:59 +0800

    重构

--- Generate_image/generate.py
+++ Generate_image/generate.py
@@ -7,9 +7,9 @@ flatNumpyArray = numpy.array(randomByteArray)
 # 根据随机字节的bytearray转换为灰度图像
 
 grayImage = flatNumpyArray.reshape(300,400)
-cv2.imwrite('RandomGray.png',grayImage)
+cv2.imwrite('./RandomGray.png',grayImage)
 
 # 根据随机字节的bytearray转换BGR图像
 bgrImage = flatNumpyArray.reshape(100,400,3)
-cv2.imwrite('RomdomColor.png',bgrImage)
+cv2.imwrite('./RomdomColor.png',bgrImage)
 
