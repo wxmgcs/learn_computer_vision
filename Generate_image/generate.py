ca1156e9f54b10593086f8ff87812c13151a5202
wxmgcs <wxmgcs@gmail.com>
2019-08-01 21:19:02 +0800

    init

---
+++ Generate_image/generate.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+import os
+
+randomByteArray = bytearray(os.urandom(120000))
+flatNumpyArray = numpy.array(randomByteArray)
+# 根据随机字节的bytearray转换为灰度图像
+
+grayImage = flatNumpyArray.reshape(300,400)
+cv2.imwrite('RandomGray.png',grayImage)
+
+# 根据随机字节的bytearray转换BGR图像
+bgrImage = flatNumpyArray.reshape(100,400,3)
+cv2.imwrite('RomdomColor.png',bgrImage)
+
--- opencv_api.py
+++ opencv_api.py
@@ -1,1 +1,13 @@
 #coding:utf-8
+import cv2
+#读取文件
+img = cv2.imread('xxxx')
+
+# 将(0,0)坐标的像素修改为白色
+img[0,0] = [255,255,255]
+
+## 保存文件
+cv2.imwrite('xxx.png',bgrImage)
+
+# 显示图像
+cv2.imshow()
