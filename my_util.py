311df17a127a2388c9384202e420ea822b07bf63
Patrick_Wang_Diyai <wxmgcs@sina.cn>
2019-08-06 23:08:52 +0800

    update

---
+++ my_util.py
@@ -0,0 +1,8 @@
+from sys import argv
+# if len(argv) < 3:
+#     print ("Usage: python "+argv[0]+" <image>")
+#     exit()
+# file_name = argv[1]
+# crop_file_name = argv[2]
+# crop_line_file_name = argv[3]
+input_filepath = "/Volumes/DATA/ctu_gitlab/vr_images/src/image_completion/test4/tripod4.png"
